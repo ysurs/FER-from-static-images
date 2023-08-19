d08e4f8f723a87c5a1717f8d2bac8a956062810a
Yash Surange <88226034+ysurs@users.noreply.github.com>
2023-08-19 23:25:09 +0530

    completing feature extractor

--- model.py
+++ model.py
@@ -8,7 +8,6 @@ class feature_extractor(nn.Module):
     def __init__(self,fe_in_channels,fe_out_channels):
         super(feature_extractor, self).__init__()
         
-        
         self.block_sequence=nn.Sequential(
             self.convolutional_block(fe_in_channels,16),
             nn.MaxPool2d(2,2),
@@ -16,11 +15,24 @@ class feature_extractor(nn.Module):
             nn.MaxPool2d(2,2),
             self.convolutional_block(32,64),
             nn.MaxPool2d(2,2),
-            self.convolutional_block(64,128),
+            self.convolutional_block(64,fe_out_channels),
             nn.MaxPool2d(2,2)
-            
         )
+        self.global_average_pooling=nn.AvgPool2d(kernel_size=10)
+        self.initialize_weights()
         
+    
+    
+    def initialize_weights(self):
+        for i in range(len(self.block_sequence)):
+            if i%2==0:
+                for layer in self.block_sequence[i]:
+                    if isinstance(layer,nn.Conv2d):
+                        nn.init.kaiming_normal_(layer.weight, mode='fan_in', nonlinearity='relu')
+                        if layer.bias is not None:
+                            nn.init.constant_(layer.bias, 0)
+        
+        return
         
         
     
@@ -36,6 +48,8 @@ class feature_extractor(nn.Module):
         
     
     def forward(self,input):
-        pass
+        extracted_features=self.block_sequence(input)
+        feature_vector=self.global_average_pooling(extracted_features).reshape(1,-1)
+        return feature_vector
     
     
