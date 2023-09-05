10cf6b151fe43c74bd759baba704d31b8da49499
Yash Surange <88226034+ysurs@users.noreply.github.com>
2023-09-05 16:36:43 +0530

    preprocessing of all images happens- presently clahe

--- preprocessing.py
+++ preprocessing.py
@@ -9,7 +9,7 @@ import dlib
 from imutils import face_utils
 from PIL import Image
 from IPython.display import display
-
+import h5py
 
 class preprocessing_image:
     
@@ -23,7 +23,7 @@ class preprocessing_image:
     
     
     def face_detection(self,image):
-        # Get the pretrained model on COCO dataset
+        # Get the pretrained model on COCO dataset- This impelementation is very slow- needs change
         detection_model=ssd.ssd300_vgg16(weights=ssd.SSD300_VGG16_Weights.DEFAULT)
         image=transforms.ToTensor()(image)
         
@@ -31,7 +31,7 @@ class preprocessing_image:
         with torch.no_grad():
             detections=detection_model([image])
         
-        x, y, x_max, y_max = detections[0]['boxes'][0].tolist()
+        x, y, x_max, y_max = detections[0]['boxes'][0].tolist()     # A person will be detected here and so we have indexed using [0]
         face = transforms.ToPILImage()(image).crop((x, y, x_max, y_max))
         face= transforms.Resize((160, 160), interpolation=Image.BILINEAR)(face)
         return np.array(face)
@@ -58,3 +58,44 @@ class preprocessing_image:
             
         return image
 
+
+if __name__ == "__main__":
+
+    # Creating an instance of the class for ckplus dataset
+    ckplus_dataset=ckplus('./CK+_Complete/')
+    
+    total_ckplus_images=[]
+    total_ckplus_labels=[]
+
+    # Getting total images and labels
+    for i in range(ckplus_dataset.__len__()):
+        total_ckplus_images.append(ckplus_dataset.__getitem__(i)[0])
+        total_ckplus_labels.append(ckplus_dataset.__getitem__(i)[1])
+    
+    # Changing into numpy arrays
+    total_ckplus_images=np.array(total_ckplus_images)
+    total_ckplus_labels=np.array(total_ckplus_labels)
+    
+    
+    # Object for preprocessing- everything from clahe, face detection
+    preprocessing_object=preprocessing_image()
+    
+    # Applying clahe to all the images
+    images_after_clahe=[]
+    
+    for i in range(total_ckplus_images.shape[0]):
+        images_after_clahe.append(preprocessing_object.clahe(total_ckplus_images[i]))
+    
+    images_after_clahe=np.array(images_after_clahe)
+    
+    
+    # Storing numpy arrays for image and labels in using h5py
+    with h5py.File('images_after_clahe.h5', 'w') as file:
+         dataset = file.create_dataset('after_clahe', data=images_after_clahe)
+    
+    with h5py.File('labels.h5', 'w') as file:
+         dataset = file.create_dataset('image_labels', data=total_ckplus_labels)
+    
+        
+    
+    
