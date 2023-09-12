7ea1c35a581bde6d942359a42a6a4baf1cc21996
Yash Surange <88226034+ysurs@users.noreply.github.com>
2023-09-12 18:38:10 +0530

    includes all preprocessing steps

--- preprocessing.py
+++ preprocessing.py
@@ -10,13 +10,25 @@ from imutils import face_utils
 from PIL import Image
 from IPython.display import display
 import h5py
+import matplotlib.pyplot as plt
+
+
+def normalise_image(x):
+  x /= 255.0
+  x -= 0.5
+  x *= 2.0
+  return x
+
 
 class preprocessing_image:
     
-    def __init__(self,clipLimit=2.0,tileGridSize=(8,8)):
+    def __init__(self,clipLimit=2.0,tileGridSize=(8,8),display_annotated=False):
         self.clahe_object=cv2.createCLAHE(clipLimit=clipLimit, tileGridSize=tileGridSize)
         self.detection_model=ssd.ssd300_vgg16(weights=ssd.SSD300_VGG16_Weights.DEFAULT)
         self.detection_model.eval()
+        self.face_detector_landmark=dlib.get_frontal_face_detector()
+        self.predictor_landmark=dlib.shape_predictor("/Users/yashsurange/Documents/GitHub/FER-from-static-images/shape_predictor_68_face_landmarks.dat")
+        self.display_annotated=display_annotated
     
     def clahe(self,image):
         image=np.array(image)
@@ -29,7 +41,6 @@ class preprocessing_image:
         
         image=transforms.ToTensor()(image)
         
-        
         with torch.no_grad():
             detections=self.detection_model([image])
         
@@ -37,28 +48,35 @@ class preprocessing_image:
         face = transforms.ToPILImage()(image).crop((x, y, x_max, y_max))
         face= transforms.Resize((160, 160), interpolation=Image.BILINEAR)(face)
         return np.array(face)
+
+    
+    def visualize_face(self,im,landmarks):
+        # Taken from https://github.com/rohan598/Landmark-Aware-Part-based-Ensemble-Transfer-Learning-Network-for-Facial-Expression-Recognition/blob/main/notebooks/summary_notebook.ipynb
+        # draw box over face
+        cv2.rectangle(im, (0,0), (160,160), (0,255,0), 2)
+        for (x,y) in landmarks:
+                cv2.circle(im, (x, y), 2, (0, 255, 0), -1)
         
+        plt.imshow(im)
+        plt.show()
+            
         
     def landmark_annotation(self,image):
-        # The image passed is a numpy array
-        # To use dlib, have figured out in check.ipynb
-        pretrained_model="shape_predictor_68_face_landmarks.dat"
-        detector = dlib.get_frontal_face_detector()
-        predictor = dlib.shape_predictor(pretrained_model)
-        
         # This detects the face in the image
-        detect_face=detector(image,0)
         
-        # At present, this we are considering only 1 image, will add support for images in batches
-        annotations=predictor(image,detect_face[0])
+        detect_face=self.face_detector_landmark(image,0)
+        try:
+            annotations=self.predictor_landmark(image,detect_face[0])
+        except:
+            print("No")
+            return
         annotations = face_utils.shape_to_np(annotations)
         
+        if self.display_annotated==True:
+            self.visualize_face(image,annotations)
         
-        ### Use below logic for actual face annotation
-        for (x,y) in annotations:
-            cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
-            
-        return image
+        annotations = annotations/160
+        return annotations
 
 
 if __name__ == "__main__":
@@ -96,9 +114,23 @@ if __name__ == "__main__":
     for i in range(images_after_clahe.shape[0]):
         face_crops.append(preprocessing_object.face_detection(images_after_clahe[i]))
     
+    # Removing images for which detection is not working : index: 1576 and 1630
+    face_crops.pop(1576)
+    face_crops.pop(1630)
+    face_crops.pop(1629)
     
+    image_landmarks=[]
+    
+    for i in range(len(face_crops)):
+        image_landmarks.append(preprocessing_object.landmark_annotation(face_crops[i]))
         
+    face_crops=np.array(face_crops).astype('float64')
     
+    normalised_images=normalise_image(face_crops)
+    
+    print(normalised_images.shape)
+    print(len(image_landmarks))
+
     
     # # Storing numpy arrays for image and labels in using h5py
     # with h5py.File('images_after_clahe.h5', 'w') as file:
